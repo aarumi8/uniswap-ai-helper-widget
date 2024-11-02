d0e0abb0ddab6fc2de8029ae9999ef3557eb821d
Max Rand <dopler168@gmail.com>
2024-11-02 23:43:35 +0300

    fix scroll behaviour

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -17,12 +17,14 @@ const Layout = ({
   <div className="font-basier-circle">
     <Head>
       <title>{title}</title>
-      <meta name="description" content={description}></meta>
+      <meta name="description" content={description} />
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <link rel="icon" href={favicon} />
     </Head>
-    <div className="min-h-screen bg-gray-50">{children}</div>
+    <div className="h-screen flex flex-col bg-gray-50">
+      {children}
+    </div>
   </div>
 )
 
